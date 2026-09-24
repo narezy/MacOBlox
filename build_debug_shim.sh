08d400ac24a046b058136a86ee6edca0b2b65692
Narezany <244472126+narezy@users.noreply.github.com>
2026-09-24 17:16:01 +0300

    Remove the signal-based profiler
    Under Darling every signal is handled through darlingserver; sampling
    the main thread froze the game within seconds.

--- build_debug_shim.sh
+++ build_debug_shim.sh
@@ -18,7 +18,7 @@ clang -target x86_64-apple-darwin -fuse-ld=lld \
   -dynamiclib -fno-objc-arc -Werror=incompatible-function-pointer-types \
   -Wl,-undefined,dynamic_lookup \
   -install_name @rpath/libMacOBloxShims.dylib \
-  "$project_dir/libMacOBloxShims.m" "$project_dir/xattr_compat.c" "$project_dir/exit_compat.c" "$project_dir/missing_symbols.c" "$project_dir/net_trace.c" "$project_dir/darling_fixes.c" "$project_dir/xfixes_raw.c" "$project_dir/dns_override.c" "$project_dir/audio_hal.c" "$project_dir/gpu_info.c" "$project_dir/gl_profile.c" "$project_dir/connectx_compat.c" "$project_dir/memory_stats.c" "$project_dir/profiler.c" "$build_dir/fast_libc.o" \
+  "$project_dir/libMacOBloxShims.m" "$project_dir/xattr_compat.c" "$project_dir/exit_compat.c" "$project_dir/missing_symbols.c" "$project_dir/net_trace.c" "$project_dir/darling_fixes.c" "$project_dir/xfixes_raw.c" "$project_dir/dns_override.c" "$project_dir/audio_hal.c" "$project_dir/gpu_info.c" "$project_dir/gl_profile.c" "$project_dir/connectx_compat.c" "$project_dir/memory_stats.c" "$build_dir/fast_libc.o" \
   -lobjc -lc++ -lc++abi -framework Foundation -framework AppKit \
   -o "$tmp_output"
 mv -- "$tmp_output" "$build_dir/libMacOBloxShims.dylib"
